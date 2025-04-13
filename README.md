a235db35f79e1eeffbf8bb8e561bbc918258026a
Dmitrii Kustov <simbiat@outlook.com>
2025-04-13 20:45:21 +0300

    Move trackers to separate libraries

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # FFXIV-Tracker
-Library to track changes of entities from Final Fantasy XIV's Lodestone
+Library to track changes of entities from Final Fantasy XIV's Lodestone.
+
+Do NOT use: WIP and can have incorrect dependencies and fail.
---
+++ src/CrossworldLinkshell.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types = 1);
+
+namespace Simbiat\FFXIV;
+
+class CrossworldLinkshell extends Linkshell
+{
+    #Custom properties
+    protected const bool crossworld = true;
+    protected string $idFormat = '/^[a-z0-9]{40}$/m';
+}
